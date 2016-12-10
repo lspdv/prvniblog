924094258ac8374c8c70faa1e50d78139b4386a0
Gabrielová, Veronika <gabrielova.veronika@gmail.com>
2016-12-10 16:35:44 +0100

    Update

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.order_by('-created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
